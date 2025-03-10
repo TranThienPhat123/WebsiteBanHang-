9b1680e4232902fd8cd284aea42387651786048e
TranThienPhat <phattranthien915@gmail.com>
2025-03-10 10:20:35 +0700

    Add project files.

---
+++ TranThienPhat_2280602326/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using TranThienPhat_2280602326.Models;
+
+namespace TranThienPhat_2280602326.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+    }
+}
---
+++ TranThienPhat_2280602326/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using TranThienPhat_2280602326.Models;
+namespace TranThienPhat_2280602326.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+}
